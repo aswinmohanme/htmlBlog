b48964afc05be93c283c753073b8624519ecce07
aswinmohanme <dev.aswinmohan@gmail.com>
2019-07-12 10:59:30 +0530

    Added font and chagne type colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
 module.exports = {
-  theme: {
-    extend: {}
-  },
-  variants: {},
-  plugins: []
+    theme: {
+        fontFamily: {
+            'sans': ['Rubik',  'sans-serif']
+        },
+        extend: {}
+    },
+    variants: {},
+    plugins: []
 }
